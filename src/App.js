d147f320216a5a65e408ccf018d4dbeb65b12542
matias marino <marinomatias084@gmail.com>
2023-04-28 17:29:17 -0300

    nuevo commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ import Banner from "./componets/Banner/Banner";
 import Title from "./componets/Title/Title";
 import ItemListContainer from "./componets/ItemListContainer/ItemListContainer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-/* import ApiPlaceholder from "./componets/ApiPlaceholder/ApiPlaceholder"; */
-/* import User from "./componets/User/User"; */
-import Products from "./componets/Products/Products";
+import ApiPlaceholder from "./componets/ApiPlaceholder/ApiPlaceholder";
+import User from "./componets/User/User";
+
 function App() {
   return (
     <BrowserRouter>
-      {/*  <User /> */}
-      <Products />
-      {/* <ApiPlaceholder /> */}
+      <User />
+
+      <ApiPlaceholder />
       <NavBar />
       <Banner />
       <Title />
       <Routes>
         <Route path="/" element={<ItemListContainer />} />
-        <Route path="/pruducts" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
--- src/componets/ItemListContainer/ItemListContainer.jsx
+++ src/componets/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Title from "../Title/Title";
 import ItemCount from "../ItemCount/ItemCount";
 
+const Productos = [
+  {
+    id: 1,
+    nombre: "almendras",
+    precio: 200,
+    img: "../img/almendra 3.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "nueces",
+    precio: 500,
+    img: "../img/nueces 3.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "pasas de uva",
+    precio: 200,
+    img: "../img/pasas negras.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "mixfrutal",
+    precio: 300,
+    img: "../img/mixfrutal3.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 5,
+    nombre: "mani",
+    precio: 200,
+    img: "../img/mani 3.jpg",
+    cantidad: 1,
+  },
+];
+
 export const ItemListContainer = ({ texto }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    await fetch("/data/productos.json")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  console.log(data);
   const onAdd = (quantity) => {
     console.log(`Compraste ${quantity} unidades`);
   };
@@ -11,6 +64,7 @@ export const ItemListContainer = ({ texto }) => {
     <>
       <Title greeting="Bienvenido" />
       <ItemCount Initial={1} stock={10} onAdd={onAdd} />
+      <Productos data={data} />
     </>
   );
 };
