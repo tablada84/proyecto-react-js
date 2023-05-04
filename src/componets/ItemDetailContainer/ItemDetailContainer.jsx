e2020a6c8997ab6dfa38a193374e6dd264798f7f
matias marino <marinomatias084@gmail.com>
2023-05-04 20:56:17 -0300

    nuevos cambios

--- src/componets/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/componets/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import ItemDetail from "../ItemDetail/ItemDetail";
-import { useParams } from "react-router-dom";
 const Productos = [
   {
     id: 1,
@@ -10,21 +9,51 @@ const Productos = [
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkcXEljq9MzQzHLsUVcUMLpQZm2u3IT8znpw&usqp=CAU",
     cantidad: 10,
   },
+  {
+    id: 2,
+    nombre: "cabernet suavignon",
+    precio: 5000,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP5VRvMTK3Kf62bUTqH7Z58yxOVVpV6ifCrA&usqp=CAU",
+    cantidad: 10,
+  },
+  {
+    id: 3,
+    nombre: "chardonne",
+    precio: 2000,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhAu4uets4SO6zIhNx2ZPM57CP8RR0_KDNVA&usqp=CAU",
+    cantidad: 10,
+  },
+  {
+    id: 4,
+    nombre: "merlot",
+    precio: 3000,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEHeaUw6HWk15f4lfj1N9c-BG2nrsXCLsVrQ&usqp=CAU",
+    cantidad: 10,
+  },
+  {
+    id: 5,
+    nombre: "toro",
+    precio: 200,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO764MRZzscfPBaukugLmAlMfApbYtJkY9wg&usqp=CAU",
+    cantidad: 10,
+  },
 ];
 
 export const ItemDetailContainer = () => {
   const [data, setData] = useState({});
-  const [detalleId] = useParams;
+
   useEffect(() => {
     const getData = new Promise((resolve) => {
       setTimeout(() => {
         resolve(Productos);
       }, 3000);
     });
-    getData.then((res) =>
-      setData(res.find((Productos) => Productos.id === parseInt(detalleId)))
-    );
-  }, [detalleId]);
+    getData.then((res) => setData(res));
+  }, []);
   return <ItemDetail data={data} />;
 };
 
